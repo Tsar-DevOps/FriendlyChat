0ac865f461896c691e29343eb28548fc3380e874
marek.zarychta@gmail.com <zarychta@google.com>
2022-09-19 06:12:24 +0200

    Committer: marek.zarychta@gmail.com <zarychta@google.com>
     On branch main
    
     Initial commit
    
     Changes to be committed:
    	new file:   __/firebase/init.js
    	new file:   __/firebase/init.json
    	new file:   favicon.ico
    	new file:   firebase-messaging-sw.js
    	new file:   images/firebase-logo.png
    	new file:   images/profile_placeholder.png
    	new file:   index.html
    	new file:   manifest.json
    	new file:   scripts/main.js
    	new file:   styles/main.css

---
+++ __/firebase/init.js
@@ -0,0 +1,10 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyA-muiOlopuvcYyW35Br88wUsZJ4Ylgoeg",
+  "appId": "1:235398661911:web:0abb705b045b8bed939952",
+  "authDomain": "friendlychat-12121.firebaseapp.com",
+  "databaseURL": "",
+  "messagingSenderId": "235398661911",
+  "projectId": "friendlychat-12121",
+  "storageBucket": "friendlychat-12121.appspot.com"
+});
